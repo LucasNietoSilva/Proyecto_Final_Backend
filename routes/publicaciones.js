2844a3701f06cd749bcc757df58b70d1624efd12
gonzamonte <gonzalomontero1995@gmail.com>
2023-03-07 22:29:26 -0300

    intento de filter

--- routes/publicaciones.js
+++ routes/publicaciones.js
@@ -4,12 +4,14 @@ const {
   mostrarPublicaciones,
   buscarPublicacion,
   agregarPublicacion,
+  filtrarPublicaciones,
 } = require("../controllers/publicaciones");
 
 const router = express.Router();
 
 router.get("/publicaciones", mostrarPublicaciones);
 router.get("/buscar/:id", buscarPublicacion);
+router.get("/publicaciones-buscar", filtrarPublicaciones);
 router.post("/publicaciones/nuevo", agregarPublicacion);
 
 module.exports = router;
